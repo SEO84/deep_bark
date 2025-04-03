2a149ec9544939234f9ee22bff25e6b75cf85358
“Ihan0316” <aijnjj12@gmail.com>
2025-04-03 10:07:18 +0900

    Flask test 수정

--- Python/FlaskProject-Deep-Bark/model.py
+++ Python/FlaskProject-Deep-Bark/model.py
@@ -1,32 +1,13 @@
 import torch
 import torch.nn as nn
 
-# CNN 모델 정의
-class CustomCNN(nn.Module):
-    def __init__(self, num_classes=2):
-        super(CustomCNN, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(64 * 56 * 56, 512)
-        self.fc2 = nn.Linear(512, num_classes)
-        self.relu = nn.ReLU()
-
-    def forward(self, x):
-        x = self.pool(self.relu(self.conv1(x)))
-        x = self.pool(self.relu(self.conv2(x)))
-        x = x.view(-1, 64 * 56 * 56)  # Flatten
-        x = self.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
-
 
 import torchvision.models as models
 # 모델 로드 함수 (ResNet50 기반 전이 학습 모델)
 def load_model(model_path="model/Dogs_classifier_model.pth"):
     model = models.resnet50(pretrained=False)
     num_ftrs = model.fc.in_features
-    model.fc = nn.Linear(num_ftrs, 62)  # Hammer / Nipper 분류 (2개 클래스)
+    model.fc = nn.Linear(num_ftrs, 62)
     model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
     model.eval()
     return model
