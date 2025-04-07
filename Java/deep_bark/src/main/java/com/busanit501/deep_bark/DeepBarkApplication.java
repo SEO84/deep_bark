75c839be0851a31bd487824e9343e73e848478b7
“Ihan0316” <aijnjj12@gmail.com>
2025-04-07 09:36:53 +0900

    Spring-Boot 수정

--- Java/deep_bark/src/main/java/com/busanit501/deep_bark/DeepBarkApplication.java
+++ Java/deep_bark/src/main/java/com/busanit501/deep_bark/DeepBarkApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaAuditing // 베이스 엔티티를 이용할수 있게, 전역에 설정하는 코드.
 public class DeepBarkApplication {
 
     public static void main(String[] args) {
