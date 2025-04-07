9c2a45e53edf4d5c050dd70f33315439c1e37e70
“Ihan0316” <aijnjj12@gmail.com>
2025-04-07 09:29:26 +0900

    Spring-Boot 세팅

---
+++ Java/deep_bark/src/main/java/com/busanit501/deep_bark/DeepBarkApplication.java
@@ -0,0 +1,13 @@
+package com.busanit501.deep_bark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepBarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepBarkApplication.class, args);
+    }
+
+}
---
+++ Java/deep_bark/src/test/java/com/busanit501/deep_bark/DeepBarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.busanit501.deep_bark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeepBarkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
